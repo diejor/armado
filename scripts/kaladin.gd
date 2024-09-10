68e6a02cdbfe9c8fd3d7d06fe7ce87ab6bc9fcad
diejor <diejorarr@gmail.com>
2024-09-09 21:59:23 -0500

    kaladin physics

--- scripts/kaladin.gd
+++ scripts/kaladin.gd
@@ -1,14 +1,15 @@
 extends RigidBody2D
 
+@export var LINEAR_FORCE = 1500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	lock_rotation = true
 	pass # Replace with function body.
 
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	var move = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-	state.linear_velocity = move * 100
+	state.apply_central_force(move*LINEAR_FORCE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
